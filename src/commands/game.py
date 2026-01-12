f2f8413d636ebde11f4dd02a4c2e415a95078ffb
gle6chik <glebbaranov21@gmail.com>
2026-01-12 23:39:42 +0700

    Remove test commands

--- src/commands/game.py
+++ src/commands/game.py
@@ -3,6 +3,5 @@ from aiogram.types import BotCommand
 def get_game_commands():
     return [
         BotCommand(command='/start', description='Перезапустить бота'),
-        BotCommand(command='/menu', description='Показать меню'),
-        BotCommand(command='/test', description='test')
+        BotCommand(command='/menu', description='Показать меню')
     ]
--- src/commands/menu.py
+++ src/commands/menu.py
@@ -3,6 +3,5 @@ from aiogram.types import BotCommand
 def get_menu_commands():
     return [
         BotCommand(command='/start', description='Перезапустить бота'),
-        BotCommand(command='/help', description='Помощь'),
-        BotCommand(command='/test', description='test')
+        BotCommand(command='/help', description='Помощь')
     ]
